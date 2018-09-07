2569d6d5c2060bde07024d9d13987ca0d656916a
Nick Bates <19batesnicholas@gmail.com>
2018-09-07 09:16:57 -0700

    Project for Unit 1

---
+++ Unit1lab/Unit1lab.py
@@ -0,0 +1,20 @@
+print ("I love python")
+myName= input ()
+print (myName)
+
+print ("My name is " + myName)
+
+print (3+2)
+
+print (3-2)
+print (5*4)
+print (9/3)
+print (3**2)
+print (15%2)
+
+num1= input()
+print (num1)
+Num2= input()
+print (Num2)
+print (int(num1)/int(Num2))
+
