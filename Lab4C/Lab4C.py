f93dc1e69954f71e1f4fb94de46dbf35a85f6895
Nick Bates <19batesnicholas@gmail.com>
2018-10-15 09:13:29 -0700

    Hi!!!

---
+++ Lab4C/Lab4C.py
@@ -0,0 +1,33 @@
+def main():
+    for i in range(0, 7):
+        draw7()
+
+def draw7():
+       starString=''
+       for i in range(0,7):
+           starString += '*'
+
+       print(starString)
+
+main()
+
+def main2():
+    for i in range (0,7):
+        starsAndStripes()
+
+def starsAndStripes():
+    newFlag=''
+    for i in range(0, 1):
+        newFlag+= "*"
+    print (newFlag)
+    for i in range (0, 1):
+        newFlag+= "-"
+    print (newFlag)
+
+main2()
+
+
+main3():
+
+
+
