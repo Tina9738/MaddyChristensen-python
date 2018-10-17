f0010b9e4f26d2ef0e2e258c2ced9494e1813cdd
Nick Bates <19batesnicholas@gmail.com>
2018-10-17 10:08:11 -0700

    Hi!!!

---
+++ Lab4D/Lab4D.py
@@ -0,0 +1,13 @@
+def main():
+    shoppingCart=[toothpaste, q-tips, milk, milk, candy, apples, paper, pencils, q-tips]
+    allinOne(shoppingCart)
+
+def allinOne(Cart):
+    print (Cart)
+
+
+
+
+
+
+main()
