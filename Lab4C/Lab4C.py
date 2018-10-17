99d616cbf01357fd2f9bd34a45ec516ce1cf9d75
Nick Bates <19batesnicholas@gmail.com>
2018-10-17 09:58:29 -0700

    Hi!!!

--- Lab4C/Lab4C.py
+++ Lab4C/Lab4C.py
@@ -11,23 +11,57 @@ def draw7():
 
 main()
 
+input ("pause")
+
 def main2():
-    for i in range (0,7):
+    for i in range (0,3):
         starsAndStripes()
 
 def starsAndStripes():
     newFlag=''
-    for i in range(0, 1):
+    flagNew=""
+    for i in range(0, 7):
         newFlag+= "*"
     print (newFlag)
-    for i in range (0, 1):
-        newFlag+= "-"
-    print (newFlag)
+    for j in range (0, 7):
+        flagNew+= "-"
+    print (flagNew)
 
 main2()
 
+input ("pause")
 
-main3():
+def main3():
+    for i in range(0,1):
+        incTriangle()
 
+def incTriangle():
+    for i in range(0,1):
+        print ("1")
+    for i in range(0,1):
+        print ("22")
+    for i in range(0,1):
+        print ("333")
+    for i in range(0,1):
+        print ("4444")
+    for i in range(0,1):
+        print ("55555")
+    for i in range(0,1):
+        print ("666666")
+    for i in range(0,1):
+        print ("7777777")
+
+main3()
+
+print ("pause")
+
+def increaseTriangle():
+    for i in range(1,20):
+        triString=""
+        for j in range(0,i):
+            triString+=str(i)
+        print(triString)
+
+increaseTriangle()
 
 
