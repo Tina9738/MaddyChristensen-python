c602ae27235f9335e9de60dc9f14941c8c120632
Nick Bates <19batesnicholas@gmail.com>
2018-10-23 09:37:46 -0700

    Hi!!!

--- Lab4D/Lab4D.py
+++ Lab4D/Lab4D.py
@@ -1,9 +1,29 @@
 def main():
-    shoppingCart=[toothpaste, q-tips, milk, milk, candy, apples, paper, pencils, q-tips]
-    allinOne(shoppingCart)
+    shoppingCart=[["toothpaste", "q-tips", "milk"], ["milk", "candy", "apples"], ["paper", "pencils", "q-tips"]]
+    print(allinOne(shoppingCart))
+
+    metalBasket=[["Q-tips", 'water', "eggs"],["Chicken"] ]
+
+
+
 
 def allinOne(Cart):
-    print (Cart)
+
+    print(Cart)
+    Shopping=[]
+    for list in Cart:
+        for items in list:
+            if items not in Shopping:
+                Shopping.append(items)
+    print(Shopping)
+
+def countQtips(Basket):
+    for lists in Basket:
+        for items in Basket:
+
+
+
+
 
 
 
